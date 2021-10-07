ba65848c75795726eb9f74796a10bc4763f89359
Bryan Whyte <bwhyte21@outlook.com>
2021-10-07 17:17:04 -0400

    Namespace name adjustment.

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API.Controllers
+namespace ProjectReactivitiesAPI.Controllers
 {
   [ApiController]
   [Route("[controller]")]
--- API/WeatherForecast.cs
+++ API/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace API
+namespace ProjectReactivitiesAPI
 {
   public class WeatherForecast
   {
