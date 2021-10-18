01788274b785c4b57cb4e36149c556a5a37e0ba9
Bryan Whyte <bwhyte21@outlook.com>
2021-10-18 19:50:17 -0400

    Updated layers with EF NuGet package and created the application's first migration. Added a programatical way to create a codefirst DB if no DBs exist and to apply any pending migrations. Next task is to seed the DB!

--- ProjectReactivities_API/Program.cs
+++ ProjectReactivities_API/Program.cs
@@ -6,21 +6,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectReactivities_DataAccess.Data;
 
 namespace ProjectReactivities_API
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      CreateHostBuilder(args).Build().Run();
-    }
+        public static void Main(string[] args)
+        {
+            /* Another way to create a DB if none exist, is to do it programatically on start, and apply any migrations available. */
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+            // Store host into a variable.
+            var host = CreateHostBuilder(args).Build();
+
+            // Once this method is complete, scope will be disposed of due to the "using" method.
+            using var scope = host.Services.CreateScope();
+
+            // Capture scope as service provider.
+            var services = scope.ServiceProvider;
+
+            try
             {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
+                // Acquire data context as a service.
+                var dataContext = services.GetRequiredService<ApplicationDbContext>();
+                // Apply any pending migrations.
+                dataContext.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                // Log any exceptions we come across.
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error has occurred during migration.");
+            }
+
+            // Don't forget to start the application!
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
+    }
 }
