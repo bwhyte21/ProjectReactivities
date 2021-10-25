53ea7e47bd74f9774c303508dbabbf19ef4be996
Bryan Whyte <bwhyte21@outlook.com>
2021-10-25 19:07:17 -0400

    Added Semantic UI React to the project.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
   // Use the useState hook to help import Activities and set the response to 'activities'.
@@ -19,16 +19,14 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {activities.map((activity: any) => (
-            <li key={activity.id}>
-              {activity.title}, {activity.venue}
-            </li>
-          ))}
-        </ul>
-      </header>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>
+            {activity.title}, {activity.venue}
+          </List.Item>
+        ))}
+      </List>
     </div>
   );
 }
