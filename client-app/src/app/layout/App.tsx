106f4e11cba73a3e1997a52857b422c5806b1ba4
Bryan Whyte <bwhyte21@outlook.com>
2021-10-29 23:53:56 -0400

    Added more folders to the structure and a navbar! Added a semantic UI navbar with a little styling! Next is to add an Activity dashboard.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,30 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
   // Use the useState hook to help import Activities and set the response to 'activities'.
-  const [activities, setActivities] = useState([]);
+  // New: now the useState is a type of Activity[], which means the ": any" can be removed from activities.map(...) due to it now having Type Safety.
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   // Use the useEffect hook to use Axios to make our API call.
+  // New: added Activity[] for TypeSafety for the response.
   useEffect(() => {
-    axios.get('https://localhost:5001/api/Activities').then((response: any) => {
+    axios.get<Activity[]>('https://localhost:5001/api/Activities').then((response) => {
       // Set the response data to activities.
       setActivities(response.data);
     });
   }, []);
 
   return (
-    <div className="App">
-      <Header as="h2" icon="users" content="Reactivities" />
-      <List>
-        {activities.map((activity: any) => (
-          <List.Item key={activity.id}>
-            {activity.title}, {activity.venue}
-          </List.Item>
-        ))}
-      </List>
-    </div>
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: '6.97385rem' }}>
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>
+              {activity.title} | {activity.venue}, {activity.city}
+            </List.Item>
+          ))}
+        </List>
+      </Container>
+    </Fragment>
   );
 }
 
---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Container, Menu } from 'semantic-ui-react';
+
+export default function NavBar() {
+    return (
+        <Menu inverted fixed='top'>
+            <Container>
+                <Menu.Item header>
+                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '0.625rem'}} />
+                    Reactivities
+                </Menu.Item>
+                <Menu.Item name='Activities' />
+                <Menu.Item>
+                    <Button positive content='Create Activity' />
+                </Menu.Item>
+            </Container>
+        </Menu>
+    );
+}
