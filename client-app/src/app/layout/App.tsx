087d435bec2c0a3506da6c526c45590d42cac912
Bryan Whyte <bwhyte21@outlook.com>
2021-11-19 01:23:30 -0500

    Added create/edit display functionality for activities. Added ability to edit activities. Added "edit form" submission functionality.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,11 +6,17 @@ import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
+  //#region Consts
+
   // Use the useState hook to help import Activities and set the response to 'activities'.
   // Now the useState is a type of Activity[], which means the ": any" can be removed from activities.map(...) due to it now having Type Safety.
   const [activities, setActivities] = useState<Activity[]>([]);
   // New: Adding " | undefined" to state that the incoming activity can be an activity OR undefined.
   const [selectedActivity, setSelctedActivity] = useState<Activity | undefined>(undefined);
+  // New: Display the status of form whether creating or editing.
+  const [editMode, setEditMode] = useState(false);
+  
+  //#endregion
 
   // Use the useEffect hook to use Axios to make our API call.
   // Added Activity[] for TypeSafety for the response.
@@ -21,26 +27,56 @@ function App() {
     });
   }, []);
 
-  function selectActivityHandler(id:string){
-      // New: As soon as an activity ("a") is matched with the incoming activity ("id"), select activity.
-      setSelctedActivity(activities.find(a => a.id === id));
+  //#region Functions
+
+  function selectActivityHandler(id: string) {
+    // New: As soon as an activity ("a") is matched with the incoming activity ("id"), select activity.
+    setSelctedActivity(activities.find((a) => a.id === id));
   }
 
-  function cancelSelectActivityHandler(){
+  function cancelSelectActivityHandler() {
     // New: set selected activity to undefined.
     setSelctedActivity(undefined);
   }
 
+  function formOpenHandler(id?: string) {
+    // If "id" is populated, select activity, else cancel.
+    id ? selectActivityHandler(id) : cancelSelectActivityHandler();
+    // Set edit mode to true.
+    setEditMode(true);
+  }
+
+  function formCloseHandler() {
+    // If an activity is being created/edited and the user clicks "cancel", close the form.
+    setEditMode(false);
+  }
+
+  
+  function upsertActivityHandler(activity: Activity){
+    // Check whether we are updating or creating an activity.  check ? updateAct : createAct.
+    activity.id ? setActivities([...activities.filter((x) => x.id !== activity.id), activity]) : setActivities([...activities, activity]); // "..." = spread operator.
+    // Set edit mode to false.
+    setEditMode(false);
+    // Set selected activity to the one just created to display successful creatrion.
+    setSelctedActivity(activity);
+  }
+
+  //#endregion
+
   return (
     <Fragment>
-      <NavBar />
+      <NavBar openForm={formOpenHandler} />
       <Container style={{ marginTop: '6.97385rem' }}>
         {/* Pass in 'activities' state into the dashboard */}
-        <ActivityDashboard 
-        activities={activities} 
-        selectedActivity = {selectedActivity}
-        selectActivity = {selectActivityHandler}
-        cancelSelectActivity = {cancelSelectActivityHandler}
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={selectActivityHandler}
+          cancelSelectActivity={cancelSelectActivityHandler}
+          editMode={editMode}
+          openForm={formOpenHandler}
+          closeForm={formCloseHandler}
+          upsertActivity={upsertActivityHandler}
         />
       </Container>
     </Fragment>
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -8,23 +8,39 @@ import ActivityList from './ActivityList';
 interface Props {
   activities: Activity[];
   selectedActivity: Activity | undefined; // Match the type of props being passed in.
-  selectActivity: (id:string) => void; // Since this function returns nothing, just add "=> void".
+  selectActivity: (id: string) => void; // Since this function returns nothing, just add "=> void".
   cancelSelectActivity: () => void; // Takes no params, returns void.
+  editMode: boolean;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  upsertActivity: (activity: Activity) => void;
 }
 
 // Destructuring the properties that we're passing into the dashboard rather than using "props: Props".
 // TL;DR: {activities}: Prop > props: Props.
-export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity }: Props) {
+export default function ActivityDashboard({
+  activities,
+  selectedActivity,
+  selectActivity,
+  cancelSelectActivity,
+  editMode,
+  openForm,
+  closeForm,
+  upsertActivity
+}: Props) {
   return (
     <Grid>
       <Grid.Column width="10">
-        <ActivityList activities={activities} selectActivity={selectActivity}/>
+        <ActivityList activities={activities} selectActivity={selectActivity} />
       </Grid.Column>
       <Grid.Column width="6">
         {/* Check if activities exist before trying to access them. */}
         {/* New: Check if any activities have been selected before trying to access them. */}
-        {selectedActivity && <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} />}
-        <ActivityForm />
+        {selectedActivity && !editMode && (
+          <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} openForm={openForm} />
+        )}
+        {/* New: We only want the current activity form component to show unless edit mode is active. */}
+        {editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity} upsertActivity={upsertActivity}/>}
       </Grid.Column>
     </Grid>
   );
