c44788bee6aa5ccf47a8ae57aca2ea9c84cefb2a
Bryan Whyte <bwhyte21@outlook.com>
2021-12-16 15:54:31 -0500

    NavLinks added!

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
-import { useStore } from '../stores/store';
 
 export default function NavBar() {
-  // Use the newly created activityStore to replace the previous handler functionality.
-  const { activityStore } = useStore();
 
   return (
     <Menu inverted fixed="top">
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} to='/' exact header>
           <img id="navLogo" src="/assets/logo.png" alt="logo" />
           Reactivities
         </Menu.Item>
-        <Menu.Item name="Activities" />
+        <Menu.Item as={NavLink} to='/activities' name="Activities" />
         <Menu.Item>
           {/* Because openForm takes an id, and Create does not provide one that exists, we pass in an empty param. */}
-          <Button onClick={() => activityStore.openForm()} positive content="Create Activity" />
+          <Button as={NavLink} to='/createActivity' positive content="Create Activity" />
         </Menu.Item>
       </Container>
     </Menu>
